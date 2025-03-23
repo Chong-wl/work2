053816a967a2f8707f22b0e11d9311633531cb1f
Chong-wl <2480654559@qq.com>
2025-03-23 22:24:27 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        Slice_o_Heaven pizzaShop = new Slice_o_Heaven();
-        pizzaShop.takeOrder("123", "Pepperoni", 20.0);
-        pizzaShop.processCardPayment("12345678901234", "12/25", 123);
-        pizzaShop.specialOfTheDay("Hawaiian Pizza", "French Fries", "10.99");
+    public static void main(String[] args) throws Exception {
+       StoreOneA store = new StoreOneA(true, 100000, 12, true);
+       store.enterStoreDetails();
+       store.showStoreInfo();
     }
 }
